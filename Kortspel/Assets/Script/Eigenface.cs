6a7effb17457ef47ee392e43aed8e0c5c72e011e
Dan Säfström <dan_anakin_25@hotmail.com>
2022-04-13 02:13:27 +0200

    Daily work on recognition
    We're getting closer to figuring out how to deal with all the functions and get a reasonable result. Only edited Eigenface.cs

--- Kortspel/Assets/Script/Eigenface.cs
+++ Kortspel/Assets/Script/Eigenface.cs
@@ -21,10 +21,19 @@ unsafe public class Eigenface : MonoBehaviour
     public Mat test;
     public Mat temp;
     public Size size;
+    public Size covarianceSize;
     public int numberOfCards;
     public int numberOfTraining;
     public double[] result;
-    public Size covarianceSize;
+    public PCA[] pCA;
+    public Mat[] testImage;
+    public Mat mean;
+    public Mat[] averageImage;
+    public Mat[] eigenVectors;
+    public Mat eig;
+    public Mat[] phi;
+    public Size imageVectorSize;
+    public Mat tmp;
 
     public Eigenface(WebCamTexture webCam, Texture2D[][] images)
     {
@@ -33,10 +42,10 @@ unsafe public class Eigenface : MonoBehaviour
         //gameObject1 = GameObject.Find("queryImage");
 
         size = new Size(images[0][0].width, images[0][0].height);
-        Size imageVectorSize = new Size(1, size.Width * size.Height);
-        Size sizu = new Size(numberOfTraining, numberOfTraining);
-        Size sizy = new Size(1, numberOfTraining * numberOfTraining);
-        covarianceSize = new Size(size.Height, size.Height);
+        imageVectorSize = new Size(1, size.Width * size.Height);
+        //Size sizu = new Size(numberOfTraining, numberOfTraining);
+        //Size sizy = new Size(1, numberOfTraining * numberOfTraining);
+        //covarianceSize = new Size(size.Height, size.Height);
         numberOfCards = images.Length;
         numberOfTraining = images[0].Length;
         result = new double[numberOfCards];
@@ -46,22 +55,25 @@ unsafe public class Eigenface : MonoBehaviour
         **********************************************************/
 
         // Initialize all matricies used for calculations
-        PCA[] pCA = new PCA[numberOfCards];
-        Mat averageImage;
+        pCA = new PCA[numberOfCards];
+        averageImage = new Mat[numberOfCards];
+        eigenVectors = new Mat[numberOfCards];
+        phi = new Mat[numberOfCards];
         Mat[] final = new Mat[numberOfCards];
         Mat eigenValues = new Mat();
-        Mat eigenVectors = new Mat();
         Mat[] A = new Mat[numberOfTraining];
         Mat[] reshapedImages = new Mat[numberOfTraining];
         Mat[] L = new Mat[numberOfCards];
         Mat[] eigenVectorsFinal = new Mat[numberOfCards];
+        mean = new Mat();
         queryImage = new Mat[numberOfCards];
         currentImage = new Mat();
         temp = new Mat();
+        testImage = new Mat[numberOfCards];
 
         for (int n = 0; n < numberOfCards; ++n)
         {
-            averageImage = Mat.Zeros(imageVectorSize, MatType.CV_64F);
+            averageImage[n] = Mat.Zeros(imageVectorSize, MatType.CV_64F);
             //L[n] = Mat.Zeros(sizy, MatType.CV_64F);
             final[n] = new Mat();
 
@@ -76,17 +88,18 @@ unsafe public class Eigenface : MonoBehaviour
                 queryImage[i].ConvertTo(queryImage[i], MatType.CV_64F);
 
                 reshapedImages[i] = queryImage[i].Reshape(0, queryImage[i].Rows*queryImage[i].Cols).Clone();
-                averageImage += reshapedImages[i];
+                averageImage[n] += reshapedImages[i];
                 
                 final[n].PushBack(reshapedImages[i].T());
                 //A.col( 0 ).copyTo( B.col(0) ); // that's fine
             }
-            Debug.Log(final[n].Cols);
-            averageImage /= numberOfCards;
 
-            Debug.Log("After: " + final[n]);
-            Mat mean = new Mat();
+            averageImage[n] /= numberOfCards;
+
+            eigenVectors[n] = new Mat();
+
             pCA[n] = new PCA(final[n], mean, PCA.Flags.DataAsCol);
+            eigenVectors[n] = pCA[n].Eigenvectors.Normalize(255, 0, NormTypes.L2);
 
             //for (int i = 0; i < numberOfTraining; ++i)
             //{
@@ -140,7 +153,7 @@ unsafe public class Eigenface : MonoBehaviour
         *********************************************************/
         
         // Initialize Eigenvector(EV) to get the correct MatType
-        Mat sum = Mat.Zeros(covarianceSize, MatType.CV_64F);
+        //Mat sum = Mat.Zeros(covarianceSize, MatType.CV_64F);
 
         //// Calculate Eigenvector(EV)
         //for (int i = 0; i < numberOfCards; ++i)
@@ -166,7 +179,7 @@ unsafe public class Eigenface : MonoBehaviour
         currentImage = OpenCvSharp.Unity.TextureToMat(Resources.Load<Texture2D>("Images/VineSkeleton/VineSkeleton"));
 
         // Resize of image for comparasion, might not be needed
-        currentImage = currentImage.Resize(covarianceSize);
+        //currentImage = currentImage.Resize(covarianceSize);
 
         // Gray scale image
         currentImage = currentImage.CvtColor(ColorConversionCodes.BGR2GRAY);
@@ -174,19 +187,40 @@ unsafe public class Eigenface : MonoBehaviour
         // Convert to correct type for comparasion
         currentImage.ConvertTo(currentImage, MatType.CV_64F);
 
+        currentImage = currentImage.Reshape(0, currentImage.Rows * currentImage.Cols).Clone();
 
+        for (int n = 0; n < numberOfCards; ++n)
+        {
+            testImage[n] = currentImage - averageImage[n];
+            phi[n] = Mat.Zeros(imageVectorSize, MatType.CV_64F);
+
+            for (int i = 0; i < numberOfTraining; ++i)
+            {
+                eig = eigenVectors[n].ColRange(i, i+1);
+                Mat temp = eig * testImage[n].T(); // Det här är fel
+                Mat temp2 = temp.T() * eig; // Och det här är fel
+                
+                phi[n] += temp2; // Den här blir fel 
+            }
+
+            tmp = testImage[n] - phi[n];
+            result[n] = tmp.Norm();
+        }
+
+        
+        
 
         // Normalized Eigenvalue of test image
-        test = currentImage - EV;
-        test = test.Normalize(255 * 64, 0, NormTypes.L2);
+        //test = currentImage - EV;
+        //test = test.Normalize(255 * 64, 0, NormTypes.L2);
 
-        // Calculate the euclidian distance for each image
-        for (int i = 0; i < numberOfCards; ++i)
-        {
-            Cv2.Pow((eigenImage[i] - test), 2, temp); // temp is a temporary Mat
-            result[i] = (double)temp.Sum(); // Explicit cast from type Scalar to double
-            result[i] = Math.Sqrt(result[i]);
-        }
+        //// Calculate the euclidian distance for each image
+        //for (int i = 0; i < numberOfCards; ++i)
+        //{
+        //    Cv2.Pow((eigenImage[i] - test), 2, temp); // temp is a temporary Mat
+        //    result[i] = (double)temp.Sum(); // Explicit cast from type Scalar to double
+        //    result[i] = Math.Sqrt(result[i]);
+        //}
 
         double min = result[0];
         int index = 0;
@@ -221,6 +255,3 @@ unsafe public class Eigenface : MonoBehaviour
         return path;
     }
 }
-
-
-
